befb0409df1c7c5d4fe78ac1b2cde0df929f5bba
Leonardo Molina <leonardoamx@outlook.com>
2026-05-20 12:34:13 -0600

    Add application theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { PrimeReactProvider } from 'primereact/api';
+import { PrimeReactProvider } from 'primereact/api'
+import 'primereact/resources/themes/lara-light-indigo/theme.css'
+import 'primereact/resources/primereact.min.css'
 import './index.css'
 import App from './App.tsx'
 
