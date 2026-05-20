d35b68ceeb21ad4ccc195be8fcc31da7b37391fa
Leonardo Molina <leonardoamx@outlook.com>
2026-05-20 16:29:10 -0600

    Add visual ripple effect

--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,13 @@ import 'primereact/resources/primereact.min.css'
 import './index.css'
 import App from './App.tsx'
 
+const primeReactOptions = {
+  ripple: true,
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <PrimeReactProvider>
+    <PrimeReactProvider value={ primeReactOptions}>
       <App />
     </PrimeReactProvider>
   </StrictMode>,
